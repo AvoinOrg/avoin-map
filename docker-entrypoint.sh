827e77f37fcb606cb9cafec5dc3dd0cc9109778d
xbexbex <xbexbex@users.noreply.github.com>
2025-02-12 12:15:11 +0000

    Update bash_history

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ -f /home/node/.bash_history ] && [ ! -f /home/node/dev/.bash_history ]; then
-  cp /home/node/.bash_history /home/node/dev/.bash_history
+if [ -f /home/node/dev/.bash_history ]; then
+    echo "History file exists"
+    cp /home/node/dev/.bash_history /home/node/.bash_history
+else
+    cp /home/node/.bash_history /home/node/dev/.bash_history
 fi
-
-# Remove the container’s .bash_history and replace it with a symlink 
-# to the file in /home/node/dev. This way, the node user’s history is 
-# always read/written from the file in the host-mounted directory.
 rm -f /home/node/.bash_history
 ln -s /home/node/dev/.bash_history /home/node/.bash_history
 
