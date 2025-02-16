c72e2c2ebce03b3d83bae1b879411851112fc6b4
xbexbex <xbexbex@users.noreply.github.com>
2025-02-16 14:20:37 +0000

    Add isAdminVerified

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/state/appletStore.ts
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/state/appletStore.ts
@@ -8,10 +8,30 @@ import { immer } from 'zustand/middleware/immer'
 
 // import { checkIsValidZoningCode } from '../common/utils'
 
-type Vars = {}
+type Vars = {
+  isAdminVerified: boolean
+}
 
-type Actions = {}
+type Actions = {
+  setIsAdminVerified: (isAdminVerified: boolean) => void
+}
 
 export const useAppletStore = create<Vars & Actions>()(
-  subscribeWithSelector(immer((set, get) => {}))
+  subscribeWithSelector(
+    immer((set, get) => {
+      const vars = {
+        isAdminVerified: false,
+      }
+
+      const actions = {
+        setIsAdminVerified: (isAdminVerified: boolean) => {
+          set((state) => {
+            state.isAdminVerified = isAdminVerified
+          })
+        },
+      }
+
+      return { ...vars, ...actions }
+    })
+  )
 )
