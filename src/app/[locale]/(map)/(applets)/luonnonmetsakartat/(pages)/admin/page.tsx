3735d8f27788d78aea66e7a3b7004841c1cd9542
xbexbex <xbexbex@users.noreply.github.com>
2025-03-04 13:40:29 +0000

    Refactor to use the BigMenuButton component

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/page.tsx
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect } from 'react'
 import Box from '@mui/material/Box'
-import { Button } from '@mui/material'
-import { styled } from '@mui/material/styles'
 import { T } from '@tolgee/react'
 
 import MutableLink from '#/components/common/MutableLink'
 import { Upload } from '#/components/icons'
+import BigMenuButton from '#/components/common/BigMenuButton'
 
 import { routeTree } from 'applets/luonnonmetsakartat/common/routes'
 
@@ -25,12 +24,7 @@ const Page = () => {
         width: '100%',
       }}
     >
-      <BigMenuButton
-        variant="contained"
-        component="label"
-        color="primary"
-        sx={{ pl: 3, pr: 3 }}
-      >
+      <BigMenuButton color="primary" sx={{ pl: 3, pr: 3 }}>
         <Box typography={'body1'}>
           <T keyName={'sidebar.admin.upload'} ns={'luonnonmetsakartat'}></T>
         </Box>
@@ -39,27 +33,5 @@ const Page = () => {
     </MutableLink>
   )
 }
-
-const BigMenuButton = styled(Button)<{ component?: string }>(({ theme }) => ({
-  width: '100%',
-  height: '60px',
-  margin: '0 0 0 0',
-  justifyContent: 'space-between',
-  borderRadius: '5px',
-  backgroundColor: '#FBFBFB',
-  border: `0.5px solid ${theme.palette.neutral.main}`,
-  boxShadow: '1px 1px 7px 0px #EEECEC',
-  '&:hover': {
-    backgroundColor: theme.palette.primary.lighter, // Light highlight on hover
-    borderColor: theme.palette.primary.main,
-  },
-  '&:active': {
-    backgroundColor: theme.palette.primary.light, // Slightly darker when clicked
-  },
-  '&:focus': {
-    outline: `2px solid ${theme.palette.primary.main}`, // Focus outline
-    outlineOffset: '2px',
-  },
-}))
 
 export default Page
