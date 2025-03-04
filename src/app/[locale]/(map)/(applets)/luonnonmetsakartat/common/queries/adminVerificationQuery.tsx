487421cf7cc43cf95c302ff9c00633281875c79b
xbexbex <xbexbex@users.noreply.github.com>
2025-03-04 11:44:16 +0000

    Add query for admin verification

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/queries/adminVerificationQuery.tsx
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/queries/adminVerificationQuery.tsx
@@ -1,19 +1,28 @@
-import { UseQueryOptions } from '@tanstack/react-query'
 import axios from 'axios'
+import { UseQueryOptions } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
+
+import { useAppletStore } from 'applets/luonnonmetsakartat/state/appletStore'
+import { AdminVerificationStatus } from 'applets/luonnonmetsakartat/common/types'
 
 const API_URL = process.env.NEXT_PUBLIC_LUONNONMETSAKARTAT_API_URL
 
 export const adminVerificationQuery = (): UseQueryOptions<boolean | null> => {
   const { data: session } = useSession()
+  const setAdminVerificationStatus =
+    useAppletStore.getState().setAdminVerificationStatus
 
   return {
-    queryKey: ['planStatsQuery', session?.user?.id],
+    queryKey: ['adminVerificationQuery', session?.user?.id],
     queryFn: async () => {
+      setAdminVerificationStatus(AdminVerificationStatus.Pending)
+
       if (session?.user?.id == null) {
+        setAdminVerificationStatus(AdminVerificationStatus.NoUser)
         return Promise.resolve(false)
       }
-      const response = await axios.get(`${API_URL}/user/plans`, {
+
+      const response = await axios.get(`${API_URL}/admin/validate`, {
         headers: {
           'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${session?.accessToken}`,
@@ -21,13 +30,24 @@ export const adminVerificationQuery = (): UseQueryOptions<boolean | null> => {
       })
 
       if (response.status === 200) {
-        return true
-      }
+        if (
+          response.data?.is_editor === true ||
+          response.data?.is_admin === true
+        ) {
+          setAdminVerificationStatus(AdminVerificationStatus.Verified)
+          return true
+        }
 
-      if (response.status === 403) {
+        setAdminVerificationStatus(AdminVerificationStatus.Rejected)
         return false
       }
 
+      if (response.status === 403) {
+        setAdminVerificationStatus(AdminVerificationStatus.Rejected)
+        return false
+      }
+
+      setAdminVerificationStatus(AdminVerificationStatus.Errored)
       return null
     },
     retry: 3,
