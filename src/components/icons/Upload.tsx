ec94fdd0778c4d4fcc2af9938df01d35a9967d61
xbexbex <xbexbex@users.noreply.github.com>
2025-03-07 13:45:58 +0000

    Update icons

--- src/components/icons/Upload.tsx
+++ src/components/icons/Upload.tsx
@@ -1,10 +1,24 @@
-import * as React from 'react'
-import type { SVGProps } from 'react'
-const SvgUpload = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={22} fill="none" {...props}>
+import React from 'react'
+import { Box, SxProps, Theme } from '@mui/material'
+
+type Props = {
+  sx?: SxProps<Theme>
+}
+
+const Upload = (props: Props) => (
+  <Box
+    component={"svg"}
+    width={16}
+    height={22}
+    viewBox="0 0 16 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <g stroke="currentColor" strokeWidth={2}>
       <path d="M1 17v4h14v-4M8 17.5V1M1.5 8 8 1.5 14.5 8" />
     </g>
-  </svg>
+  </Box>
 )
-export default SvgUpload
+
+export default Upload
