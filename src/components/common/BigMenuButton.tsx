39873927275dd82d86a56c528bbabca94e09fe0c
xbexbex <xbexbex@users.noreply.github.com>
2025-03-09 14:36:19 +0000

    Update BigMenuButton

--- src/components/common/BigMenuButton.tsx
+++ src/components/common/BigMenuButton.tsx
@@ -21,6 +21,7 @@ const BigMenuButton = ({
       {...buttonProps} // This spreads all other props, allowing them to override defaults
       sx={[
         {
+          typography: 'body1',
           width: '100%',
           height: '60px',
           margin: '0 0 0 0',
@@ -52,7 +53,7 @@ const BigMenuButton = ({
             outline: 'none !important',
             border: `0.5px solid ${theme.palette.primary.main}`,
             boxShadow: '1px 1px 7px 0px #EEECEC',
-          },    
+          },
         },
         ...(Array.isArray(sx) ? sx : sx ? [sx] : []),
       ]}
