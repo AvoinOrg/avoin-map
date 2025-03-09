740f1dfa1cc02cb85c820cb0b83fd2a1de86a168
xbexbex <xbexbex@users.noreply.github.com>
2025-03-09 14:36:06 +0000

    Add placeholder FeatureProperties

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/types.ts
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/types.ts
@@ -5,3 +5,12 @@ export enum AdminVerificationStatus {
   NoUser = 'noUser',
   Errored = 'errored',
 }
+
+export interface FeatureProperties {
+  id: string
+  name: string | number
+  area_ha: number
+  zoning_code: string | null
+  old_zoning_code?: string
+  old_id?: string | number
+}
