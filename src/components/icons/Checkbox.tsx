1189c780df5621ec9007639da4e372fe6e4f8d75
xbexbex <xbexbex@users.noreply.github.com>
2025-03-13 10:43:49 +0000

    Update checkbox icons

--- src/components/icons/Checkbox.tsx
+++ src/components/icons/Checkbox.tsx
@@ -1,9 +1,31 @@
-import * as React from 'react'
-import type { SVGProps } from 'react'
-const SvgCheckbox = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} fill="none" {...props}>
-    <path fill="#F4F4F4" d="M0 0h23v23H0z" />
-    <path stroke="#A0A0A0" strokeWidth={2} d="M1 1h21v21H1z" />
-  </svg>
+import React from 'react'
+import { Box, SxProps, Theme } from '@mui/material'
+
+type Props = {
+  sx?: SxProps<Theme>
+}
+
+const Checkbox = (props: Props) => (
+  <Box
+    component={'svg'}
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <rect x="1" y="1" width="22" height="22" rx="2" />
+    <rect
+      x="1"
+      y="1"
+      width="22"
+      height="22"
+      rx="2"
+      stroke="#A0A0A0"
+      strokeWidth="2"
+    />
+  </Box>
 )
-export default SvgCheckbox
+
+export default Checkbox
