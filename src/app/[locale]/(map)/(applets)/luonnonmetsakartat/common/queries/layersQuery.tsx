5c60791caba43e0fdfbbe5880cd692ffaa860336
xbexbex <xbexbex@users.noreply.github.com>
2025-03-14 13:36:38 +0000

    Add layersQuery

---
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/queries/layersQuery.tsx
@@ -0,0 +1,100 @@
+import { FetchStatus } from '#/common/types/general'
+import { UseQueryOptions } from '@tanstack/react-query'
+import axios from 'axios'
+import { FeatureCollection } from 'geojson'
+import { area as turfArea } from '@turf/turf'
+
+import { useAppletStore } from '#/app/[locale]/(map)/(applets)/luonnonmetsakartat/state/appletStore'
+import { useSession } from 'next-auth/react'
+import { AdminLayerConf, LayerConfState } from '../types'
+
+const API_URL = process.env.NEXT_PUBLIC_LUONNONMETSAKARTAT_API_URL
+
+// export const layerQuery = (
+//   layerId: string
+// ): UseQueryOptions<LayerConf | null> => {
+//   const { data: session } = useSession()
+//   const updateAdminLayerConf = useAppletStore.getState().updateAdminLayerConf
+//   const addAdminLayerConf = useAppletStore.getState().addAdminLayerConf
+
+//   return {
+//     queryKey: ['layer', layerId],
+//     queryFn: async () => {
+//       // Get layer data from API
+//       const response = await axios.get(`${API_URL}/layer/${layerId}`, {
+//         headers: {
+//           Authorization: `Bearer ${session?.accessToken}`,
+//         },
+//       })
+
+//       if (response.status === 200) {
+//         // Map API response to AdminLayerConf
+//         const layerConf: AdminLayerConf = {
+//           id: response.data.id,
+//           name: response.data.name,
+//           description: response.data.description || '',
+//           isVisible: !response.data.is_hidden, // Note the inversion of is_hidden to isVisible
+//           state: LayerConfState.Idle,
+//           createdTs: response.data.created_ts * 1000, // Convert to milliseconds
+//           updatedTs: response.data.updated_ts * 1000, // Convert to milliseconds
+//         }
+
+//         // Check if this layer already exists in the store
+//         const adminLayerConfs = useAppletStore.getState().adminLayerConfs
+//         const existingLayer = adminLayerConfs.find(
+//           (conf) => conf.id === layerId
+//         )
+
+//         if (existingLayer) {
+//           // Update existing layer
+//           updateAdminLayerConf(layerConf)
+//         } else {
+//           // Add new layer
+//           addAdminLayerConf(layerConf)
+//         }
+
+//         return layerConf
+//       }
+
+//       return null
+//     },
+//     retry: 3,
+//     retryDelay: (attemptIndex) => Math.min(attemptIndex * 1000, 3000),
+//     staleTime: 60 * 1000, // 1 minute
+//     refetchOnWindowFocus: false,
+//   }
+// }
+
+// For getting all layers (if needed)
+export const layersQuery = (): UseQueryOptions<AdminLayerConf[]> => {
+  const setLayerConfs = useAppletStore.getState().setLayerConfs
+
+  return {
+    queryKey: ['layers'],
+    queryFn: async () => {
+      const response = await axios.get(`${API_URL}/layers`)
+
+      if (response.status === 200) {
+        const layerConfs: AdminLayerConf[] = response.data.map(
+          (layer: any) => ({
+            id: layer.id,
+            name: layer.name,
+            description: layer.description || '',
+            createdTs: layer.created_ts * 1000,
+            updatedTs: layer.updated_ts * 1000,
+          })
+        )
+
+        setLayerConfs(layerConfs)
+
+        return layerConfs
+      }
+
+      return []
+    },
+    retry: 3,
+    retryDelay: (attemptIndex) => Math.min(attemptIndex * 1000, 3000),
+    staleTime: 60 * 1000, // 1 minute
+    refetchOnWindowFocus: false,
+  }
+}
