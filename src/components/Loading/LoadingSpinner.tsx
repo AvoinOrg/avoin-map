52dc77158fa7a8d5d0861e737491dba8001f9a0a
xbexbex <xbexbex@users.noreply.github.com>
2025-03-14 11:57:18 +0000

    Update LoadingSpinner

--- src/components/Loading/LoadingSpinner.tsx
+++ src/components/Loading/LoadingSpinner.tsx
@@ -1,6 +1,35 @@
 import React from 'react'
-import { CircularProgress, SxProps, Theme } from '@mui/material'
+import {
+  CircularProgress,
+  CircularProgressProps,
+  SxProps,
+  Theme,
+} from '@mui/material'
 
-export const LoadingSpinner = ({ sx }: { sx?: SxProps<Theme> }) => {
-  return <CircularProgress sx={[...(Array.isArray(sx) ? sx : [sx])]} />
+interface LoadingSpinnerProps extends Omit<CircularProgressProps, 'ref'> {
+  sx?: SxProps<Theme>
+}
+
+export const LoadingSpinner = ({
+  sx,
+  size = "4rem",
+  color = 'primary',
+  variant = 'indeterminate',
+  thickness = 3.6,
+  disableShrink = false,
+  value = 0,
+  ...rest
+}: LoadingSpinnerProps) => {
+  return (
+    <CircularProgress
+      size={size}
+      color={color}
+      variant={variant}
+      thickness={thickness}
+      disableShrink={disableShrink}
+      value={value}
+      sx={[...(Array.isArray(sx) ? sx : [sx])]}
+      {...rest}
+    />
+  )
 }
