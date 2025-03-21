d7b335da6b7139492ade3912ef2273905194a546
xbexbex <xbexbex@users.noreply.github.com>
2025-03-21 10:58:48 +0000

    Update deps

--- src/common/store/userStore.ts
+++ src/common/store/userStore.ts
@@ -1,19 +1,28 @@
 'use client'
 
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 
-import { User } from '#/common/types/state'
+import { UserAuth, UserAuthState, UserDataState } from '#/common/types/state'
 
 interface Vars {
-  user: User | null
+  userAuth: UserAuth | null
+  userData: User | null
+  userAuthState: UserAuthState
+  userDataState: UserDataState
   signOutActions: Record<string, () => void>
 }
 
 interface Actions {
-  setUser: (user: User | null) => void
+  setUserData: (userData: User | null) => void
+  setUserAuth: (userAuth: UserAuth | null) => void
+  setUserAuthState: (userAuthState: UserAuthState) => void
+  setUserDataState: (userDataState: UserDataState) => void
   addSignOutAction: (key: string, action: () => void) => void
   removeSignOutAction: (key: string) => void
+  signOut: () => void
 }
 
 type State = Vars & Actions
@@ -21,14 +30,32 @@ type State = Vars & Actions
 export const useUserStore = create<State>()(
   immer((set, get) => {
     const vars: Vars = {
-      user: null,
+      userAuth: null,
+      userData: null,
+      userAuthState: UserAuthState.Unauthenticated,
+      userDataState: UserDataState.Unfetched,
       signOutActions: {},
     }
 
     const actions: Actions = {
-      setUser: (user: User | null) => {
+      setUserData: (userData: User | null) => {
         set((state) => {
-          state.user = user
+          state.userData = userData
+        })
+      },
+      setUserAuth: (userAuth: UserAuth | null) => {
+        set((state) => {
+          state.userAuth = userAuth
+        })
+      },
+      setUserAuthState: (userAuthState: UserAuthState) => {
+        set((state) => {
+          state.userAuthState = userAuthState
+        })
+      },
+      setUserDataState: (userDataState: UserDataState) => {
+        set((state) => {
+          state.userDataState = userDataState
         })
       },
       addSignOutAction: (key: string, action: () => void) => {
@@ -41,6 +68,12 @@ export const useUserStore = create<State>()(
           set((state) => {
             delete state.signOutActions[key]
           })
+        }
+      },
+      signOut: () => {
+        for (const key in get().signOutActions) {
+          get().signOutActions[key]()
+          signOut()
         }
       },
     }
