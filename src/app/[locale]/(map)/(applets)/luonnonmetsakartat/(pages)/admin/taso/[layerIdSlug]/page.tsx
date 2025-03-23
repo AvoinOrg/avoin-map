1cb9d0167564d7aa714f4e2c3503d2d963700115
xbexbex <xbexbex@users.noreply.github.com>
2025-03-23 14:04:07 +0000

    Add placeholder page and layout for layer

---
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/taso/[layerIdSlug]/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import React, { useRef, useEffect, useState, ChangeEvent, use } from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import { useParams, useRouter } from 'next/navigation'
+import { buffer } from '@turf/turf'
+import booleanValid from '@turf/boolean-valid'
+import { flattenDeep } from 'lodash-es'
+import { useTranslate } from '@tolgee/react'
+import { Feature, FeatureCollection } from 'geojson'
+
+import { getRoute } from '#/common/utils/routing-client'
+import { getGeoJsonArea } from '#/common/utils/gis'
+import { generateUUID } from '#/common/utils/general'
+import BigMenuButton from '#/components/common/BigMenuButton'
+import { Upload } from '#/components/icons'
+
+import {
+  FeatureProperties,
+  LayerConfState,
+} from '#/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/types'
+import { routeTree } from '#/app/[locale]/(map)/(applets)/luonnonmetsakartat/common/routes'
+import LayerImportShp from 'applets/luonnonmetsakartat/components/LayerImportShp'
+import { useAppletStore } from '#/app/[locale]/(map)/(applets)/luonnonmetsakartat/state/appletStore'
+import { layerPostMutation } from 'applets/luonnonmetsakartat/common/queries/layerPostMutation'
+import { useMutation } from '@tanstack/react-query'
+import { LoadingSpinner } from '#/components/Loading'
+import { SidebarContentBox } from '#/components/Sidebar'
+import { set } from 'ol/transform'
+
+const Page = () => {
+  const [isLayerReady, setIsLayerReady] = useState(false)
+  const [isAreaCollectionReady, setIsAreaCollectionReady] = useState(false)
+  const params = useParams<{ layerIdSlug: string }>()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const router = useRouter()
+  const { t } = useTranslate('luonnonmetsakartat')
+
+  const adminLayerConf = useAppletStore(
+    (state) => state.adminLayerConfs[params.layerIdSlug]
+  )
+  const layerAreaCollection = useAppletStore(
+    (state) => state.layerAreaCollections[params.layerIdSlug]
+  )
+  // const localAdminLayerPostMutation = useMutation(adminLayerPostMutation())
+
+  useEffect(() => {
+    if (adminLayerConf && adminLayerConf.state === LayerConfState.Idle) {
+      setIsLayerReady(true)
+    } else {
+      setIsLayerReady(false)
+    }
+    console.log(adminLayerConf)
+  }, [adminLayerConf])
+
+  useEffect(() => {
+    if (
+      layerAreaCollection &&
+      layerAreaCollection.state === LayerConfState.Idle
+    ) {
+      setIsAreaCollectionReady(true)
+    } else {
+      setIsAreaCollectionReady(false)
+    }
+    console.log(layerAreaCollection)
+  }, [layerAreaCollection])
+
+  return (
+    <>
+      <SidebarContentBox>
+        {!isLayerReady && (
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <LoadingSpinner></LoadingSpinner>
+          </Box>
+        )}
+        {isLayerReady && (
+          <Typography variant="h6">{adminLayerConf.name}</Typography>
+        )}
+      </SidebarContentBox>
+    </>
+  )
+}
+
+export default Page
