3014fd375fd0ea54c6e880309009b1791662f26a
xbexbex <xbexbex@users.noreply.github.com>
2025-04-01 10:56:08 +0000

    Reformat

--- src/components/icons/CheckboxChecked.tsx
+++ src/components/icons/CheckboxChecked.tsx
@@ -25,7 +25,7 @@ const CheckboxChecked = (props: Props) => (
       stroke="black"
       strokeWidth="2"
     />
-    <path d="M6 11.5L11 17L19 8" stroke="black" stroke-width="2" />
+    <path d="M6 11.5L11 17L19 8" stroke="black" strokeWidth="2" />
   </Box>
 )
 
