2898ad201a15558101d3e41de94e2fd9263ca7fe
xbexbex <xbexbex@users.noreply.github.com>
2025-04-01 10:56:17 +0000

    Add loadingHorizontal

---
+++ src/components/Loading/LoadingHorizontal.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import { SxProps, Theme } from '@mui/material'
+
+type Props = {
+  sx?: SxProps<Theme>
+}
+
+const LoadingHorizontal = (props: Props) => (
+  <Box
+    component="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <style>
+      {`
+        .spinner_S1WN{
+          animation: spinner_MGfb .8s linear infinite;
+          animation-delay: -.8s
+        }
+        .spinner_Km9P{
+          animation-delay: -.65s
+        }
+        .spinner_JApP{
+          animation-delay: -.5s
+        }
+        @keyframes spinner_MGfb{
+          93.75%, 100%{
+            opacity: .2
+          }
+        }
+      `}
+    </style>
+    <circle className="spinner_S1WN" cx="4" cy="12" r="3" fill="currentColor" />
+    <circle
+      className="spinner_S1WN spinner_Km9P"
+      cx="12"
+      cy="12"
+      r="3"
+      fill="currentColor"
+    />
+    <circle
+      className="spinner_S1WN spinner_JApP"
+      cx="20"
+      cy="12"
+      r="3"
+      fill="currentColor"
+    />
+  </Box>
+)
+
+export default LoadingHorizontal
