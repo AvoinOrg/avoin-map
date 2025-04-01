a49771752741b128d6e8ce356c9161287b5c2a83
xbexbex <xbexbex@users.noreply.github.com>
2025-04-01 10:53:41 +0000

    Update layer layout

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/taso/[layerIdSlug]/layoutClient.tsx
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/taso/[layerIdSlug]/layoutClient.tsx
@@ -10,27 +10,33 @@ import { adminLayerQuery } from 'applets/luonnonmetsakartat/common/queries/admin
 import { adminLayerAreaQuery } from 'applets/luonnonmetsakartat/common/queries/adminLayerAreaQuery'
 import { useMapStore } from '#/common/store/mapStore'
 import { useDoesLayerGroupExist } from '#/common/hooks/map/useDoesLayerGroupExist'
-import { getLayerGroupId } from 'applets/luonnonmetsakartat/common/utils'
-import { SerializableLayerGroupAddOptions } from '#/common/types/map'
+import {
+  createAdminLayerConf,
+  getLayerGroupId,
+} from 'applets/luonnonmetsakartat/common/utils'
+import { LayerGroupAddOptions } from '#/common/types/map'
 import { Feature } from 'geojson'
-import { FeatureProperties } from 'applets/luonnonmetsakartat/common/types'
+import {
+  FeatureProperties,
+  LayerConfState,
+} from 'applets/luonnonmetsakartat/common/types'
 import { getGeoJsonArea } from '#/common/utils/gis'
 
 const layoutClient = ({ children }: { children: React.ReactNode }) => {
   const params = useParams<{ layerIdSlug: string }>()
-  const enableSerializableLayerGroup = useMapStore(
-    (state) => state.enableSerializableLayerGroup
-  )
-  const addSerializableLayerGroup = useMapStore(
-    (state) => state.addSerializableLayerGroup
-  )
-  const disableSerializableLayerGroup = useMapStore(
-    (state) => state.disableSerializableLayerGroup
-  )
+  const enableLayerGroup = useMapStore((state) => state.enableLayerGroup)
+  const addLayerGroup = useMapStore((state) => state.addLayerGroup)
+  const disableLayerGroup = useMapStore((state) => state.disableLayerGroup)
   const adminLayerConf = useStore(
     useAppletStore,
     (state) => state.adminLayerConfs[params.layerIdSlug]
   )
+  const layerAreaCollection = useStore(
+    useAppletStore,
+    (state) => state.layerAreaCollections[params.layerIdSlug]
+  )
+  const adminApiKey = useStore(useAppletStore, (state) => state.adminApiKey)
+
   const { refetch: layerRefetch } = useQuery({
     ...adminLayerQuery(params.layerIdSlug),
     enabled: false, // This prevents the query from running automatically
@@ -53,7 +59,7 @@ const layoutClient = ({ children }: { children: React.ReactNode }) => {
     const init = async () => {
       if (adminLayerConf && !isLoaded.current && doesLayerGroupExist != null) {
         const layerGroupId = getLayerGroupId(params.layerIdSlug)
-        const layerGroupAddOptions: SerializableLayerGroupAddOptions = {
+        const layerGroupAddOptions: LayerGroupAddOptions = {
           zoomToExtent: true,
           // dataUpdateMutator: async (data: FeatureCollection) => {
           //   if (updatePlanConf != null) {
@@ -62,47 +68,48 @@ const layoutClient = ({ children }: { children: React.ReactNode }) => {
           //     console.error('Unable to add dataUpdateMutator')
           //   }
           // },
-          drawOptions: {
-            idField: 'id',
-            polygonEnabled: true,
-            editEnabled: true,
-            deleteEnabled: true,
-            // featureAddMutator: (feature: Feature) => {
-            //   const properties: FeatureProperties = {
-            //     id: generateUUID(),
-            //     name: '',
-            //     area_ha: getGeoJsonArea(feature) / 10000,
-            //     zoning_code: '',
-            //   }
+          // drawOptions: {
+          //   idField: 'id',
+          //   polygonEnabled: true,
+          //   editEnabled: true,
+          //   deleteEnabled: true,
+          //   // featureAddMutator: (feature: Feature) => {
+          //   //   const properties: FeatureProperties = {
+          //   //     id: generateUUID(),
+          //   //     name: '',
+          //   //     area_ha: getGeoJsonArea(feature) / 10000,
+          //   //     zoning_code: '',
+          //   //   }
 
-            //   feature.properties = properties
+          //   //   feature.properties = properties
 
-            //   return feature
-            // },
-            featureUpdateMutator: (feature: Feature) => {
-              const properties = feature.properties as FeatureProperties
-              const newProperties: FeatureProperties = {
-                ...properties,
-                areaHa: getGeoJsonArea(feature) / 10000,
-              }
+          //   //   return feature
+          //   // },
+          //   featureUpdateMutator: (feature: Feature) => {
+          //     const properties = feature.properties as FeatureProperties
+          //     const newProperties: FeatureProperties = {
+          //       ...properties,
+          //       areaHa: getGeoJsonArea(feature) / 10000,
+          //     }
 
-              feature.properties = newProperties
+          //     feature.properties = newProperties
 
-              return feature
-            },
-          },
+          //     return feature
+          //   },
+          // },
         }
 
         if (doesLayerGroupExist) {
-          await enableSerializableLayerGroup(layerGroupId, layerGroupAddOptions)
+          await enableLayerGroup(layerGroupId, layerGroupAddOptions)
         } else {
-          const layerConf = createLayerConf(
-            planConf.data,
-            planConf.id,
-            ZONING_CODE_COL
+          const layerConf = createAdminLayerConf(
+            adminApiKey as string,
+            // layerAreaCollection,
+            adminLayerConf.id,
+            adminLayerConf.colorCode
           )
 
-          await addSerializableLayerGroup(layerGroupId, {
+          await addLayerGroup(layerGroupId, {
             ...layerGroupAddOptions,
             layerConf: layerConf,
           })
@@ -113,42 +120,34 @@ const layoutClient = ({ children }: { children: React.ReactNode }) => {
     }
 
     if (
-      planConf &&
-      !planConf.isHidden &&
-      ![PlanConfState.FETCHING, PlanConfState.DELETING].includes(
-        planConf.state || PlanConfState.IDLE
+      adminLayerConf &&
+      ![LayerConfState.Fetching, LayerConfState.Deleting].includes(
+        adminLayerConf.state
       ) &&
       !isLoaded.current &&
-      globalState === GlobalState.IDLE &&
       doesLayerGroupExist != null
     ) {
       init()
       // return () => {
       //   try {
-      //     disableSerializableLayerGroup(layerGroupId)
+      //     disableLayerGroup(layerGroupId)
       //   } catch (e) {
       //     // if it fails, the layer is (most likely) already disabled/removed
       //   }
       // }
-    } else if (!planConf && doesLayerGroupExist) {
-      disableSerializableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(
-        () => {}
-      )
-    } else if (planConf && planConf.isHidden && doesLayerGroupExist) {
-      disableSerializableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(
-        () => {}
-      )
+    } else if (!adminLayerConf && doesLayerGroupExist) {
+      disableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(() => {})
+      // } else if (adminLayerConf && adminLayerConf.isHidden && doesLayerGroupExist) {
+      //   disableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(() => {})
     } else if (
-      planConf &&
-      planConf.state != null &&
-      planConf.state === PlanConfState.FETCHING
+      adminLayerConf &&
+      adminLayerConf.state != null &&
+      adminLayerConf.state === LayerConfState.Fetching
     ) {
-      disableSerializableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(
-        () => {}
-      )
+      disableLayerGroup(getLayerGroupId(params.layerIdSlug)).catch(() => {})
       isLoaded.current = false
     }
-  }, [planConf, isLoaded, doesLayerGroupExist, globalState])
+  }, [adminLayerConf, isLoaded, doesLayerGroupExist])
 
   useEffect(() => {
     if (adminLayerConf) {
