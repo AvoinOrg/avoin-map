234536ab281aef454477cca05cf4d85d4306a4fd
xbexbex <xbexbex@users.noreply.github.com>
2025-05-10 13:40:57 +0000

    Fix viewbox

--- src/components/icons/Descending.tsx
+++ src/components/icons/Descending.tsx
@@ -10,9 +10,9 @@ const SvgDescending = (props: Props) => (
     component="svg"
     xmlns="http://www.w3.org/2000/svg"
     width={23}
-    height={22}
+    height={24}
     fill="none"
-    viewBox="0 0 23 22"
+    viewBox="0 0 23 24"
     {...props}
   >
     <path d="M0 1H23M0 21H10M0 10.4118H16.5" stroke="black" strokeWidth="2" />
