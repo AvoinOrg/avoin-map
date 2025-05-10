a93a688bd6ac63c65f3cfc72e68de9edb889e76b
xbexbex <xbexbex@users.noreply.github.com>
2025-05-10 12:26:53 +0000

    Update search icon

--- src/components/icons/Search.tsx
+++ src/components/icons/Search.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
-import type { SVGProps } from 'react'
-const SvgSearch = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={26} height={26} fill="none" {...props}>
+import { Box } from '@mui/system'
+
+type Props = {
+  sx?: any
+}
+
+const SvgSearch = (props: Props) => (
+  <Box
+    component="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    width={26}
+    height={26}
+    fill="none"
+    viewBox="0 0 26 26"
+    {...props}
+  >
     <g stroke="currentColor" strokeWidth={2}>
-      <circle cx={8.171} cy={8.171} r={7.171} transform="matrix(.71045 -.70375 .71045 .70375 0 11.5)" />
+      <circle
+        cx={8.171}
+        cy={8.171}
+        r={7.171}
+        transform="matrix(.71045 -.70375 .71045 .70375 0 11.5)"
+      />
       <path d="m17 17 8 8" />
     </g>
-  </svg>
+  </Box>
 )
+
 export default SvgSearch
