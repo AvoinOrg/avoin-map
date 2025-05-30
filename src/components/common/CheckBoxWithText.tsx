8263ede6126b7f53ae408bc898f97476c542d7b4
xbexbex <xbexbex@users.noreply.github.com>
2025-05-30 14:33:38 +0000

    Update CheckBoxWithText

--- src/components/common/CheckBoxWithText.tsx
+++ src/components/common/CheckBoxWithText.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
-import { Box, Checkbox, Typography, SxProps, Theme } from '@mui/material'
+import {
+  Checkbox,
+  Typography,
+  SxProps,
+  Theme,
+  FormControlLabel, // Import FormControlLabel
+} from '@mui/material'
 
 import CheckboxIcon from '#/components/icons/Checkbox'
 import CheckboxCheckedIcon from '#/components/icons/CheckboxChecked'
@@ -7,16 +13,19 @@ import CheckboxCheckedIcon from '#/components/icons/CheckboxChecked'
 interface CheckBoxWithTextProps {
   checked: boolean
   onChange: (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.SyntheticEvent<Element, Event>,
     checked: boolean
   ) => void
   children?: React.ReactNode
-  sx?: SxProps<Theme>
+  sx?: SxProps<Theme> // For the entire FormControlLabel wrapper
+  checkboxSx?: SxProps<Theme> // Specific sx for the Checkbox component itself
   iconSx?: SxProps<Theme>
   iconCheckedSx?: SxProps<Theme>
   textSx?: SxProps<Theme>
   disabled?: boolean
   required?: boolean
+  // Allow any other props to be passed to the underlying MUI Checkbox
+  [key: string]: any
 }
 
 const CheckBoxWithText = ({
@@ -24,75 +33,81 @@ const CheckBoxWithText = ({
   onChange,
   children,
   sx,
+  checkboxSx,
   iconSx,
   iconCheckedSx,
   textSx,
   disabled = false,
   required = false,
-  ...checkboxProps
+  ...rest // Captures other props for the MUI Checkbox (e.g., name, value)
 }: CheckBoxWithTextProps) => {
   return (
-    <Box
+    <FormControlLabel
       sx={[
         {
+          m: 0,
           display: 'flex',
           flexDirection: 'row',
           alignItems: 'center',
-          mb: 1,
+          '&:hover': {
+            cursor: disabled ? 'not-allowed' : 'pointer',
+          },
         },
         ...(Array.isArray(sx) ? sx : [sx]),
       ]}
-    >
-      <Checkbox
-        checked={checked}
-        onChange={onChange}
-        disabled={disabled}
-        required={required}
-        icon={
-          <CheckboxIcon
-            sx={[
-              {
-                backgroundColor: 'neutral.light',
-              },
-              ...(Array.isArray(iconSx) ? iconSx : [iconSx]),
-            ]}
-          />
-        }
-        checkedIcon={
-          <CheckboxCheckedIcon
-            sx={[
-              {
-                backgroundColor: '#97C68B',
-              },
-              ...(Array.isArray(iconCheckedSx)
-                ? iconCheckedSx
-                : [iconCheckedSx]),
-            ]}
-          />
-        }
-        sx={[
-          {
-            p: 0,
-            mr: '1rem',
-            color: 'white',
-          },
-        ]}
-        {...checkboxProps}
-      />
-      <Typography
-        variant="body2"
-        sx={[
-          {
-            color: 'neutral.darker',
-            userSelect: 'none',
-          },
-          ...(Array.isArray(textSx) ? textSx : [textSx]),
-        ]}
-      >
-        {children}
-        {required && ' *'}
-      </Typography>
-    </Box>
+      control={
+        <Checkbox
+          icon={
+            <CheckboxIcon
+              sx={[
+                {
+                  backgroundColor: 'neutral.light', // Default icon style
+                },
+                ...(Array.isArray(iconSx) ? iconSx : [iconSx]), // User-provided icon styles
+              ]}
+            />
+          }
+          checkedIcon={
+            <CheckboxCheckedIcon
+              sx={[
+                {
+                  backgroundColor: '#97C68B',
+                },
+                ...(Array.isArray(iconCheckedSx) // User-provided checked icon styles
+                  ? iconCheckedSx
+                  : [iconCheckedSx]),
+              ]}
+            />
+          }
+          sx={[
+            {
+              p: 0, // Maintain zero padding from original Checkbox style
+            },
+            ...(Array.isArray(checkboxSx) ? checkboxSx : [checkboxSx]), // User-provided Checkbox styles
+          ]}
+          {...rest} // Spread other props like 'name', 'value', 'inputProps'
+        />
+      }
+      label={
+        <Typography
+          variant="body2"
+          sx={[
+            {
+              color: 'neutral.darker',
+              userSelect: 'none',
+              ml: 2,
+            },
+            ...(Array.isArray(textSx) ? textSx : [textSx]),
+          ]}
+        >
+          {children}
+          {required && ' *'}
+        </Typography>
+      }
+      checked={checked}
+      onChange={onChange}
+      disabled={disabled}
+    />
   )
 }
 
