35c24113599f9fa8909b5684ca03674bde017853
xbexbex <xbexbex@users.noreply.github.com>
2025-06-06 11:39:26 +0000

    Rename MapPopupModal to MapPopup

--- src/components/Map/MapPopup.tsx
+++ src/components/Map/MapPopup.tsx
@@ -1,86 +1,89 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Box from '@mui/material/Box'
-import { styled } from '@mui/material/styles'
-// import { MapGeoJSONFeature } from 'maplibre-gl'
-// import Feature from 'ol/Feature'
-// import { PopupOpts } from '#/common/types/map'
-import { PopupOpts } from '#/common/types/map'
+import { Modal } from '@mui/material'
 
-interface Props {
-  popupOpts: PopupOpts | null
-}
+import { useMapStore } from '#/common/store/mapStore'
 
-export const MapPopup = ({ popupOpts }: Props) => {
-  const [popupFeatures, setPopupFeatures] = React.useState<any[]>()
+export const MapPopupModal = () => {
+  const [isActive, setIsActive] = useState(false)
+  const activePopupData = useMapStore((state) => state.activePopupData)
+  const removeSelectedFeatures = useMapStore(
+    (state) => state.removeSelectedFeatures
+  )
+
+  const popupData = useMemo(() => {
+    if (!activePopupData || activePopupData.length === 0) {
+      return null
+    }
+
+    const newPopupData = activePopupData[0]
+    return newPopupData
+  }, [activePopupData])
 
   useEffect(() => {
-    if (!popupOpts) {
-      setPopupFeatures(undefined)
-    } else {
-      // check if the object is an ol feature
-      //@ts-ignore
-      if (popupOpts.features[0].getProperties) {
-        //@ts-ignore
-        setPopupFeatures(popupOpts.features.map((f) => f.getProperties()))
-      } else {
-        setPopupFeatures(popupOpts.features)
-      }
+    console.log(popupData)
+    if (!popupData) {
+      setIsActive(false)
+      return
     }
-  }, [popupOpts])
+
+    setIsActive(true)
+  }, [popupData])
+
+  const handleClose = () => {
+    setIsActive(false)
+    if (popupData?.features) {
+      removeSelectedFeatures({ features: popupData.features })
+    }
+  }
 
   return (
-    //   <Box
-    //   ref={mapRef}
-    //   id="map"
-    //   className="ol-map"
-    //   sx={{
-    //     position: 'absolute',
-    //     top: 0,
-    //     bottom: 0,
-    //     width: '100vw',
-    //     height: '100vh',
-    //     '.ol-scale-line': { right: '8px', left: 'auto', bottom: '26px' },
-    //   }}
-    // ></Box>
     <>
-      {popupOpts && (
-        <PopupContainer className="popup-container">
-          {popupFeatures && <popupOpts.PopupElement features={popupFeatures}></popupOpts.PopupElement>}
-        </PopupContainer>
+      {popupData && popupData.type === 'modal' && (
+        <Modal
+          open={isActive}
+          onClose={handleClose}
+          aria-labelledby="map-popup-modal-title"
+          aria-describedby="map-popup-modal-description"
+          slotProps={{
+            backdrop: {
+              sx: {
+                backgroundColor: 'transparent',
+              },
+            },
+          }}
+          sx={{
+            position: 'absolute' as const,
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: {
+              xs: '90%',
+              sm: '70%',
+              md: '500px',
+            },
+            maxHeight: '90vh',
+            bgColor: 'transparent',
+            backgroundColor: 'transparent',
+            boxShadow: 24,
+            borderRadius: 5,
+            outline: 'none',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Box sx={{ display: 'flex', flex: 1, height: '100%', width: '100%' }}>
+            {popupData && (
+              <popupData.component
+                features={popupData.features}
+                onClose={handleClose}
+              />
+            )}
+          </Box>
+        </Modal>
       )}
     </>
   )
 }
-
-const PopupContainer = styled('div')(({ theme }) => ({
-  // position: 'absolute',
-  padding: '15px',
-  // bottom: '12px',
-  // left: '-50px',
-  minWidth: '200px',
-  maxWidth: '550px',
-  display: 'flex',
-  flexDirection: 'column',
-  overflowY: 'auto',
-  overflowX: 'hidden',
-  // '&:after, &:before': {
-  //   top: '100%',
-  //   border: 'solid transparent',
-  //   content: "' '",
-  //   height: 0,
-  //   width: 0,
-  //   position: 'absolute',
-  //   pointerEvents: 'none',
-  // },
-
-  // '&:after': {
-  //   borderTopColor: 'white',
-  //   borderWidth: '10px',
-  //   left: '48px',
-  //   marginLeft: '-10px',
-  // },
-
-  // '&:before': { borderTopColor: '#cccccc', borderWidth: '11px', left: '48px', marginLeft: '-11px' },
-}))
