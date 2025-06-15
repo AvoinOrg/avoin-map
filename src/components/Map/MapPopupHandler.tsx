7fc14e321fb7ae092e7a21d53c94cc7591f27b7d
xbexbex <xbexbex@users.noreply.github.com>
2025-06-15 14:56:49 +0000

    Add extra props to popupData component

--- src/components/Map/MapPopupHandler.tsx
+++ src/components/Map/MapPopupHandler.tsx
@@ -65,6 +65,7 @@ export const MapPopupHandler = () => {
               <popupData.component
                 features={popupData.features}
                 onClose={handleClose}
+                {...popupData.componentProps}
               />
             )}
           </Box>
