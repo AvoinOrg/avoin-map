c7d4fa0efce9ec39b4955a1925fd736bb58b9112
xbexbex <xbexbex@users.noreply.github.com>
2025-06-16 12:09:19 +0000

    Update wrapping styles

--- src/components/Map/MapModalWrapper.tsx
+++ src/components/Map/MapModalWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { ReactNode, useMemo } from 'react'
+import React, { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 import Box from '@mui/material/Box'
 import { SxProps, Theme } from '@mui/material'
 import { useUIStore } from '#/common/store'
@@ -17,21 +17,60 @@ export const MapModalWrapper = ({
   minWidthBeforeFullScreen = 500,
 }: MapModalWrapperProps) => {
   const minMapDims = useUIStore((state) => state.mapDims.min)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [viewMode, setViewMode] = useState<
+    'constrained' | 'fullscreen' | 'full-height'
+  >('constrained')
+
+  useEffect(() => {
+    const element = wrapperRef.current
+    if (!element || !minMapDims) {
+      return
+    }
+
+    let observer: ResizeObserver | undefined
+
+    const checkViewMode = () => {
+      if (minMapDims.width <= minWidthBeforeFullScreen) {
+        setViewMode('fullscreen')
+        return
+      }
+      if (element.offsetHeight >= minMapDims.height - 1) {
+        setViewMode('full-height')
+        return
+      }
+
+      setViewMode('constrained')
+    }
+
+    checkViewMode()
+
+    observer = new ResizeObserver(checkViewMode)
+    observer.observe(element)
+
+    return () => {
+      if (observer) {
+        observer.disconnect()
+      }
+    }
+  }, [minMapDims, wrapperRef.current, minWidthBeforeFullScreen])
 
   return (
     <>
       {minMapDims && (
         <Box
           className="map-modal-wrapper"
+          ref={wrapperRef}
           sx={[
             {
               position: 'absolute',
               outline: 'none',
               display: 'flex',
               flexDirection: 'column',
+              maxHeight: minMapDims.height,
             },
             // Conditional styles
-            minMapDims.width > minWidthBeforeFullScreen
+            viewMode === 'constrained'
               ? {
                   maxWidth: minMapDims.width,
                   left: minMapDims.centerX,
@@ -39,7 +78,9 @@ export const MapModalWrapper = ({
                   transform: 'translate(-50%, -50%)',
                   boxShadow: 24,
                 }
-              : {
+              : null,
+            viewMode === 'fullscreen'
+              ? {
                   maxWidth: '100%',
                   width: '100%',
                   height: '100%',
@@ -48,8 +89,23 @@ export const MapModalWrapper = ({
                     flexGrow: 1,
                     minHeight: 0,
                     minWidth: '0 !important', // to allow it to scale down if it has minWidth set when drawn over the map as modal
+                    borderRadius: '0 !important',
                   },
-                },
+                }
+              : null,
+            viewMode === 'full-height'
+              ? {
+                  maxWidth: minMapDims.width,
+                  left: minMapDims.centerX,
+                  top: minMapDims.centerY,
+                  transform: 'translate(-50%, -50%)',
+                  boxShadow: 24,
+                  '& > *': {
+                    borderRadius: '0 !important',
+                  },
+                }
+              : null,
+
             // Merge with sx prop passed to the component
             ...(Array.isArray(sx) ? sx : [sx]),
           ]}
