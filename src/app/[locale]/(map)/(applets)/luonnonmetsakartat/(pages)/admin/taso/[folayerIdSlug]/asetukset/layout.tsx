843f7b9b29f9d44a221910efa4cc9e4a6f7bf3b3
xbexbex <xbexbex@users.noreply.github.com>
2025-07-02 14:40:10 +0000

    Update page titles

--- src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/taso/[folayerIdSlug]/asetukset/layout.tsx
+++ src/app/[locale]/(map)/(applets)/luonnonmetsakartat/(pages)/admin/taso/[folayerIdSlug]/asetukset/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import LayoutClient from './layoutClient'
 
 export const metadata: Metadata = {
-  title: 'Luonnonmetsakartat - Admin / Karttatason asetukset',
+  title: 'Luonnonmetsakartat / Admin - Karttatason asetukset',
 }
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
