daf974b2ed17f23d4a085f94af91991feb43c508
xbexbex <xbexbex@users.noreply.github.com>
2025-07-06 12:38:51 +0000

    Update map actions layout

--- src/components/Map/MapActionsWrapper.tsx
+++ src/components/Map/MapActionsWrapper.tsx
@@ -1,24 +1,30 @@
 import { Box } from '@mui/material'
 import { MapButtons } from './MapButtons'
 import { MapSearchBar } from './MapSearchBar'
+import { useUIStore } from '#/common/store'
 
 export const MapActionsWrapper = () => {
+  const minMapWidth = useUIStore((state) => state.mapDims.min?.width)
+  const isVertical = minMapWidth != null && minMapWidth < 500
+
   return (
     <Box
+      className="map-actions-wrapper"
       sx={(theme) => ({
         position: 'absolute',
-        top: theme.spacing(1),
-        right: theme.spacing(1),
+        top: theme.spacing(2),
+        right: theme.spacing(2),
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: isVertical ? 'column' : 'row',
         gap: theme.spacing(1),
-        alignItems: 'flex-start',
+        alignItems: 'flex-end',
+        flex: 1,
         zIndex:
           theme.zIndex.mobileStepper /* force this to be on top of the map */,
       })}
     >
       <MapSearchBar />
-      <MapButtons />
+      <MapButtons isVertical={isVertical} />
     </Box>
   )
 }
