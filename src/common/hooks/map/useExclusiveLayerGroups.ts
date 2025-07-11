311bbca8e31c9fa6c2e2050a5a80c36b2a48b828
xbexbex <xbexbex@users.noreply.github.com>
2025-07-11 12:25:53 +0000

    Add order levels to useExclusiveLayerGroups

--- src/common/hooks/map/useExclusiveLayerGroups.ts
+++ src/common/hooks/map/useExclusiveLayerGroups.ts
@@ -1,22 +1,33 @@
 import { useEffect, useRef } from 'react'
 import { useMapStore } from '#/common/store/mapStore'
 import { useVisibleLayerGroupIds } from './useVisibleLayerGroupIds'
+import { LayerOrderLevel } from '#/common/types/map'
 
 // This hook toggles off layer groups upon mount, and on upon unmount, except for those specified in `excludedLayerGroupIds`.
 // Useful for when a page needs to show specific layers.
 export const useExclusiveLayerGroups = (
-  excludedLayerGroupIds: string[] = []
+  excludedLayerGroupIds: string[] = [],
+  layerOrderLevelsToApply: LayerOrderLevel[] = [
+    LayerOrderLevel.LAYER,
+    LayerOrderLevel.OVERLAY,
+  ]
 ) => {
   const disableLayerGroup = useMapStore((state) => state.disableLayerGroup)
   const enableLayerGroup = useMapStore((state) => state.enableLayerGroup)
 
   const visibleLayerGroupIds = useVisibleLayerGroupIds()
+  const layerGroups = useMapStore((state) => state._layerGroups)
   const previouslyVisibleLayerGroupIds = useRef<string[]>([])
 
   useEffect(() => {
-    const layersToDisable = visibleLayerGroupIds.filter(
-      (id) => !excludedLayerGroupIds.includes(id)
-    )
+    const layersToDisable = visibleLayerGroupIds.filter((id) => {
+      const layerGroupOrderLevel = layerGroups[id]?.orderLevel
+      if (layerOrderLevelsToApply.includes(layerGroupOrderLevel)) {
+        return !excludedLayerGroupIds.includes(id)
+      }
+
+      return false
+    })
     previouslyVisibleLayerGroupIds.current = layersToDisable
 
     layersToDisable.forEach((id) => {
