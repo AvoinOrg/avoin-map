7cc9f33e586bec82b76a497ef0951f02eaca6078
xbexbex <xbexbex@users.noreply.github.com>
2025-07-15 15:02:34 +0000

    Update defaultListedLayerGroups

--- src/components/Map/layers/defaultListedLayerGroups.ts
+++ src/components/Map/layers/defaultListedLayerGroups.ts
@@ -1,25 +1,24 @@
 import { LayerOrderLevel, ListedLayerGroup } from '#/common/types/map'
-import {
-  osmBackgroundLayerGroupId,
-  osmBackgroundLayerConf,
-} from '#/components/Map/layers/common/OSM/background'
+import { osmBackgroundLayerConf } from '#/components/Map/layers/common/OSM/background'
+
+export const listedOsmBackgroundLayerGroup: ListedLayerGroup = {
+  id: osmBackgroundLayerConf.id,
+  addOptions: {
+    layerConf: osmBackgroundLayerConf,
+    layerOrderOptions: {
+      layerOrderLevel: LayerOrderLevel.BACKGROUND,
+      disableOthersInGroup: true,
+    },
+  },
+  // orderLevel: LayerOrderLevel.BACKGROUND,
+  name: 'OpenStreetMap',
+  translationNs: 'avoin-map',
+  nameTranslationKey: 'layers.osm.description',
+  thumbnail: '/files/img/layer-thumbnails/osm/background.jpg',
+  // description: 'Base layers for the map',
+  // descriptionTranslationKey: 'layers.osm.description',
+}
 
 export const defaultListedLayerGroups: ListedLayerGroup[] = [
-  {
-    id: osmBackgroundLayerGroupId,
-    addOptions: {
-      layerConf: osmBackgroundLayerConf,
-      layerOrderOptions: {
-        layerOrderLevel: LayerOrderLevel.BACKGROUND,
-        disableOthersInGroup: true,
-      },
-    },
-    // orderLevel: LayerOrderLevel.BACKGROUND,
-    name: 'OpenStreetMap',
-    translationNs: 'avoin-map',
-    nameTranslationKey: 'layers.osm.description',
-    thumbnail: '/files/img/layer-thumbnails/osm/background.jpg',
-    // description: 'Base layers for the map',
-    // descriptionTranslationKey: 'layers.osm.description',
-  },
+  listedOsmBackgroundLayerGroup,
 ]
