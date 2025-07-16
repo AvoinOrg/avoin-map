9048d6eef417cab33dde68baaac78e8f7b99f8d3
xbexbex <xbexbex@users.noreply.github.com>
2025-07-16 12:00:10 +0000

    Add mml layers

---
+++ src/components/Map/layers/common/MML/kiinteistotunnukset.ts
@@ -0,0 +1,34 @@
+import { ExpressionSpecification } from 'maplibre-gl'
+
+import { LayerConf, ExtendedStyleSpecification } from '#/common/types/map'
+
+export const layerGroupId: string = 'mml_kiinteistotunnukset'
+
+const getStyle = async (): Promise<ExtendedStyleSpecification> => {
+  return {
+    version: 8,
+    name: layerGroupId,
+    sources: {
+      [layerGroupId]: {
+        type: 'raster',
+        tiles: [`/api/map/mml/tms/{z}/{x}/{y}?layer=kiinteistotunnukset`],
+        tileSize: 256,
+        attribution:
+          '<a href="https://www.maanmittauslaitos.fi/avoindata">© Maanmittauslaitos</a>',
+      },
+    },
+    layers: [
+      {
+        id: 'mml_kiinteistotunnukset_layer',
+        source: layerGroupId,
+        type: 'raster',
+        paint: {},
+      },
+    ],
+  }
+}
+
+export const mmlKiinteistotunnuksetLayerConf: LayerConf = {
+  id: layerGroupId,
+  style: getStyle,
+}
