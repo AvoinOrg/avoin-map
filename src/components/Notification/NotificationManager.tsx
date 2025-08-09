b21d4e34a95d3a5a13622d733db0f8f49984439b
xbexbex <xbexbex@users.noreply.github.com>
2025-08-09 14:51:58 +0000

    Update to use typography

--- src/components/Notification/NotificationManager.tsx
+++ src/components/Notification/NotificationManager.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { closeSnackbar, useSnackbar } from 'notistack'
 
 import { useUIStore } from '#/common/store'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { Cross } from '../icons'
 
 const NotificationManager = () => {
@@ -15,28 +15,35 @@ const NotificationManager = () => {
       Object.values(notifications).forEach((notification) => {
         if (!notification.shown) {
           updateNotification(notification.id, { shown: true })
-          enqueueSnackbar(notification.message, {
-            variant: notification.variant || 'default',
-            autoHideDuration:
-              notification.duration || notification.manualDismiss ? null : 6000,
-            persist: notification.manualDismiss,
-            hideIconVariant: true,
-            action: (key) => (
-              <Box
-                onClick={() => {
-                  closeSnackbar(key)
-                }}
-                sx={{
-                  height: '100%',
-                  '&:hover': { cursor: 'pointer' },
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <Cross sx={{ display: 'flex', height: '16px' }}></Cross>
-              </Box>
-            ),
-          })
+          enqueueSnackbar(
+            <Typography sx={{ whiteSpace: 'pre-line' }}>
+              {notification.message}
+            </Typography>,
+            {
+              variant: notification.variant || 'default',
+              autoHideDuration:
+                notification.duration || notification.manualDismiss
+                  ? null
+                  : 6000,
+              persist: notification.manualDismiss,
+              hideIconVariant: true,
+              action: (key) => (
+                <Box
+                  onClick={() => {
+                    closeSnackbar(key)
+                  }}
+                  sx={{
+                    height: '100%',
+                    '&:hover': { cursor: 'pointer' },
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Cross sx={{ display: 'flex', height: '16px' }}></Cross>
+                </Box>
+              ),
+            }
+          )
         }
       })
     }
