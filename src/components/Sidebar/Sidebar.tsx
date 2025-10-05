aca02fa0f31d0d8bf29209517922aeb0afa0e1d2
xbexbex <xbexbex@users.noreply.github.com>
2025-10-05 14:16:13 +0000

    Refactor to use SidebarDrawerContainer

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,12 +5,11 @@ import { Box, SxProps, Theme } from '@mui/material'
 
 import { useUIStore } from '#/common/store'
 // import { MapPopup } from '../Map/MapPopup_old'
-import { useMapStore } from '#/common/store'
 import Drawer from './Drawer'
-import PopupDrawer from './PopupDrawer'
 import { SidebarHeader, SidebarToggleButton } from '#/components/Sidebar'
 import { Navbar } from './Navbar'
 import { LoadingSpinner } from '../Loading'
+import { SidebarDrawerContainer } from './SidebarDrawerContainer'
 
 export const Sidebar = ({
   headerElement,
@@ -24,8 +23,7 @@ export const Sidebar = ({
   children: React.ReactNode
 }) => {
   const isSidebarOpen = useUIStore((state) => state.isSidebarOpen)
-  const isMapPopupOpen = useUIStore((state) => state.isMapPopupOpen)
-  const setIsMapPopupOpen = useUIStore((state) => state.setIsMapPopupOpen)
+  const isSidebarDrawerOpen = useUIStore((state) => state.isSidebarDrawerOpen)
   const setSidebarWidth = useUIStore((state) => state.setSidebarWidth)
   const isNavbarHidden = useUIStore((state) => state.isNavbarHidden)
   const isSidebarLoading = useUIStore((state) => state.isSidebarLoading)
@@ -53,101 +51,91 @@ export const Sidebar = ({
   return (
     <Box
       ref={sidebarRef}
-      className="sidebar-container"
-      sx={[
-        {
-          zIndex: 'drawer',
-          // backgroundColor: 'white',
-          minHeight: 0,
-          width: 'max-content',
-          maxWidth: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          position: 'relative',
-          flex: 1,
-          pointerEvents: isSidebarOpen || isMapPopupOpen ? 'auto' : 'none',
-        },
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
+      sx={{
+        zIndex: 'drawer',
+        display: 'flex',
+        minWidth: 0,
+        width: '100%',
+        flexDirection: 'row',
+        maxWidth: '100%',
+        position: 'relative',
+        minHeight: 0,
+        flex: 1,
+        pointerEvents: 'none',
+      }}
     >
-      <SidebarToggleButton
-        sx={(theme) => ({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          zIndex: theme.zIndex.drawer + 11,
-          pointerEvents: 'auto',
-        })}
-      />
       <Box
-        sx={{ display: 'flex', flexDirection: 'row', flex: 1, minHeight: 0 }}
+        className="sidebar-container"
+        sx={[
+          {
+            minHeight: 0,
+            minWidth: 0,
+            flex: '1',
+            maxWidth: '30rem',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            pointerEvents:
+              isSidebarOpen || isSidebarDrawerOpen ? 'auto' : 'none',
+          },
+          ...(Array.isArray(sx) ? sx : [sx]),
+        ]}
       >
-        <Drawer open={isSidebarOpen}>
-          {headerElement ? (
-            headerElement
-          ) : (
-            <SidebarHeader title={'avoin map'}></SidebarHeader>
-          )}
-          {isSidebarLoading && (
+        <SidebarToggleButton
+          sx={(theme) => ({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: theme.zIndex.drawer + 11,
+            pointerEvents: 'auto',
+          })}
+        />
+        <Box
+          sx={{ display: 'flex', flexDirection: 'row', flex: 1, minHeight: 0 }}
+        >
+          <Drawer open={isSidebarOpen}>
+            {headerElement ? (
+              headerElement
+            ) : (
+              <SidebarHeader title={'avoin map'}></SidebarHeader>
+            )}
+            {isSidebarLoading && (
+              <Box
+                sx={(theme) => ({
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  zIndex: theme.zIndex.drawer + 10,
+                  borderRadius: 'inherit', // Inherit border radius from parent if needed
+                })}
+              >
+                <LoadingSpinner size="5rem" />
+              </Box>
+            )}
             <Box
-              sx={(theme) => ({
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                zIndex: theme.zIndex.drawer + 10,
-                borderRadius: 'inherit', // Inherit border radius from parent if needed
-              })}
-            >
-              <LoadingSpinner size="5rem" />
-            </Box>
-          )}
-          <Box
-            ref={sidebarRef}
-            sx={[
-              {
-                overflow: 'auto',
-                display: 'flex',
-                flexGrow: 1,
-                maxWidth: '100vw',
-                backgroundColor: 'neutral.lighter',
-              },
-            ]}
-          >
-            {children}
-          </Box>
-          {!isNavbarHidden &&
-            (navbarElement ? navbarElement : <Navbar></Navbar>)}
-        </Drawer>
-
-        <PopupDrawer open={isMapPopupOpen}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <Box
-              sx={{
-                textDecoration: 'none',
-                alignSelf: 'flex-end',
-                margin: '10px 10px 0 0',
-                '&:after': {
-                  content: "'✖'",
+              sx={[
+                {
+                  overflow: 'auto',
+                  display: 'flex',
+                  flexGrow: 1,
+                  maxWidth: '100vw',
+                  backgroundColor: 'neutral.lighter',
                 },
-              }}
-              onClick={() => setIsMapPopupOpen(false)}
-            />
-            {/* <MapPopup popupOpts={popupOpts} /> */}
-          </Box>
-        </PopupDrawer>
-      </Box>
-      {/* {mode === 'full' && (
+              ]}
+            >
+              {children}
+            </Box>
+            {!isNavbarHidden &&
+              (navbarElement ? navbarElement : <Navbar></Navbar>)}
+          </Drawer>
+        </Box>
+        {/* {mode === 'full' && (
         <Box
           sx={{
             display: 'flex',
@@ -159,6 +147,8 @@ export const Sidebar = ({
           {children}
         </Box>
       )} */}
+      </Box>
+      <SidebarDrawerContainer></SidebarDrawerContainer>
     </Box>
   )
 }
