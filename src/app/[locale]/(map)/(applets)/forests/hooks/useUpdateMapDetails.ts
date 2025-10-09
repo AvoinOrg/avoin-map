48ae19eedd98b8cf39b05994d27ff955ebda1479
xbexbex <xbexbex@users.noreply.github.com>
2025-10-09 11:43:27 +0000

    Update useUpdateMapDetails cloneDeep

--- src/app/[locale]/(map)/(applets)/forests/hooks/useUpdateMapDetails.ts
+++ src/app/[locale]/(map)/(applets)/forests/hooks/useUpdateMapDetails.ts
@@ -7,24 +7,35 @@ import {
 } from '../utils'
 import { ForestryMethod, LayerLevel } from '../types'
 import { useMapStore } from '#/common/store'
+import { cloneDeep } from 'lodash-es'
 
 export const useUpdateMapDetails = () => {
   const setLayoutProperty = useMapStore((state) => state.setLayoutProperty)
   const setPaintProperty = useMapStore((state) => state.setPaintProperty)
 
-  const updateMapDetails = (forestryMethod: ForestryMethod, carbonBalanceDifferenceFlag: boolean) => {
+  const updateMapDetails = (
+    forestryMethod: ForestryMethod,
+    carbonBalanceDifferenceFlag: boolean
+  ) => {
     const co2eValueExpr = fiForestsSumMethodAttrs(forestryMethod, 'cbt')
 
-    const fiForestsRelativeCO2eValueExpr = fiForestsBestMethodVsOther(forestryMethod, 'cbt')
+    const fiForestsRelativeCO2eValueExpr = fiForestsBestMethodVsOther(
+      forestryMethod,
+      'cbt'
+    )
 
     const fillColor = carbonBalanceDifferenceFlag
       ? fiForestsAreaCO2FillColor(fiForestsRelativeCO2eValueExpr)
       : fiForestsAreaCO2FillColor(co2eValueExpr)
 
     for (const type of Object.keys(layerOptions)) {
-      setPaintProperty(`${type}-fill`, 'fill-color', fillColor)
+      setPaintProperty(`${type}-fill`, 'fill-color', cloneDeep(fillColor))
     }
-    setLayoutProperty(LayerLevel.Parcel + '-symbol', 'text-field', fiForestsTextfieldExpression(co2eValueExpr))
+    setLayoutProperty(
+      LayerLevel.Parcel + '-symbol',
+      'text-field',
+      cloneDeep(fiForestsTextfieldExpression(co2eValueExpr))
+    )
   }
 
   return updateMapDetails
