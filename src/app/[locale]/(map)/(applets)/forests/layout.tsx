8561ddd21392eb59ed6791b5290b1d0b6cc25ce6
xbexbex <xbexbex@users.noreply.github.com>
2025-10-25 12:17:34 +0000

    Update layout

--- src/app/[locale]/(map)/(applets)/forests/layout.tsx
+++ src/app/[locale]/(map)/(applets)/forests/layout.tsx
@@ -3,12 +3,30 @@
 import React from 'react'
 
 import AppletWrapper from '#/components/common/AppletWrapper'
-import { Sidebar } from '#/components/Sidebar'
+import { BreadcrumbNav, Sidebar, SidebarHeader } from '#/components/Sidebar'
+import { routeTree } from 'map/common/routes'
+import { defaultListedLayerGroups } from '#/components/Map/layers/defaultListedLayerGroups'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <AppletWrapper mapContext={'fi_forests'}>
-      <Sidebar>{children}</Sidebar>
+    <AppletWrapper
+      mapContext={'forests'}
+      isNavbarHidden={true}
+      localizationNamespace={'fi-forests'}
+      listedLayerGroups={defaultListedLayerGroups}
+    >
+      <Sidebar
+        headerElement={
+          <SidebarHeader title={'avoin map'}>
+            <BreadcrumbNav
+              collapseIfRoot={true}
+              routeTree={routeTree}
+            ></BreadcrumbNav>
+          </SidebarHeader>
+        }
+      >
+        {children}
+      </Sidebar>
     </AppletWrapper>
   )
 }
