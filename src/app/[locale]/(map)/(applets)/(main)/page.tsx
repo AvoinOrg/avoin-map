a8e8e94a9f0f6f972e8106ce01f1eca69e6f8975
xbexbex <xbexbex@users.noreply.github.com>
2025-10-26 15:34:49 +0000

    Add default fitBounds

--- src/app/[locale]/(map)/(applets)/(main)/page.tsx
+++ src/app/[locale]/(map)/(applets)/(main)/page.tsx
@@ -1,12 +1,29 @@
 /** @jsxImportSource @emotion/react */
 'use client'
 
+import { useEffect } from 'react'
+
+import { useMapStore } from '#/common/store'
 import AppletWrapper from '#/components/common/AppletWrapper'
 import { defaultListedLayerGroups } from '#/components/Map/layers/defaultListedLayerGroups'
+import { FINLAND_BOUNDS } from '#/common/constants/map'
 
 import { Menu } from './components/Menu'
 
 const Page = () => {
+  const fitBounds = useMapStore((state) => state.fitBounds)
+
+  useEffect(() => {
+    fitBounds({
+      bbox: FINLAND_BOUNDS,
+      options: { duration: 200, lonExtra: 1 },
+      autoRelocateOptions: {
+        checkIfAutoRelocate: true,
+        disableAutoRelocate: true,
+      },
+    })
+  }, [fitBounds])
+
   return (
     <AppletWrapper
       mapContext={'main'}
