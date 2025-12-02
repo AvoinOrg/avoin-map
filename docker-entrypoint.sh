7adce224698ba066bc63059ba0c080680a3856e0
xbexbex <xbexbex@users.noreply.github.com>
2025-12-02 14:19:15 +0000

    Update dev env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-if [ "$NODE_ENV" = "production" ]; then
-    yarn install --only=prod && node start;
+
+if [ -f /home/node/dev/.bash_history ]; then
+    echo "History file exists"
+    cp /home/node/dev/.bash_history /home/node/.bash_history
 else
-    yarn install && yarn dev;
+    cp /home/node/.bash_history /home/node/dev/.bash_history
+fi
+rm -f /home/node/.bash_history
+ln -s /home/node/dev/.bash_history /home/node/.bash_history
+
+
+if [ "$NODE_ENV" = "production" ]; then
+    yarn install --only=prod && yarn run prebuild && yarn build && yarn start;
+else
+    yarn install && yarn run prebuild && yarn dev;
 fi
