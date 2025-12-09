70b1385b36adfd1dbf2791bcff054a9143d94b48
xbexbex <xbexbex@users.noreply.github.com>
2025-12-09 15:43:51 +0000

    Update to use draw instance

--- src/common/hooks/map/useDrawMode.ts
+++ src/common/hooks/map/useDrawMode.ts
@@ -1,18 +1,20 @@
 import { useMapStore } from '#/common/store/mapStore'
+import { getActiveDrawInstance } from '#/common/store/mapStore/mapDrawSlice'
 import { getDrawMode } from '#/common/utils/map'
 
 export const useDrawMode = () => {
   const options = useMapStore((state) => state._drawOptions)
 
-  if (options.draw == null || options.isEnabled === false) {
+  if (options.isEnabled === false) {
     return null
   }
 
-  if (options.currentMode) {
-    return options.currentMode
-  }
+  const draw = getActiveDrawInstance()
+  if (!draw) return null
 
-  const MaplibreDrawMode = options.draw.getMode() as any
+  if (options.currentMode) return options.currentMode
+
+  const MaplibreDrawMode = draw.getMode() as any
 
   return getDrawMode(MaplibreDrawMode)
 }
