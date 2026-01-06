5aa13006c279907b19c1c9dd7dda954d452e1cc2
xbexbex <xbexbex@users.noreply.github.com>
2026-01-06 13:16:08 +0000

    Add useIsMobile

---
+++ src/common/hooks/ui/useIsMobile.ts
@@ -0,0 +1,7 @@
+import { useMediaQuery, useTheme } from '@mui/material'
+import type { Breakpoint } from '@mui/material/styles'
+
+export const useIsMobile = (breakpoint: Breakpoint = 'md') => {
+  const theme = useTheme()
+  return useMediaQuery(theme.breakpoints.down(breakpoint))
+}
